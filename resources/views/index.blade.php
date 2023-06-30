915cdc0374b05ca3dc1663a9907043ca3bf937c9
Shuhad Abdul Azeez <shuhad04@gmail.com>
2023-06-30 10:15:04 +0530

    modified the page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,13 +3,21 @@
 @section('title', 'The list of tasks')
 
 @section('content')
-    <!-- @if (count($tasks)) -->
+    <nav class="mb-4">
+        <a href="{{ route('tasks.create') }}" class="link">Add Task</a>
+    </nav>
+    
     @forelse ($tasks as $task)
         <div>
-            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+            <a href="{{ route('tasks.show', ['task' => $task->id]) }}" @class(['line-through' => $task->id])>{{ $task->title }}</a>
         </div>
     @empty
         <div>There are no tasks!</div>
     @endforelse
-    <!-- @endif -->
+
+    @if ($tasks->count())
+        <nav class="mb-4">
+            {{ $tasks->links() }}
+        </nav> 
+    @endif
 @endsection
